06dae7d44a97c4d9bd972f3964818f084dc46dcb
noorxhk <irfanusman220@gmail.com>
2021-11-14 16:51:49 +0500

    make responsive infoForm

--- src/InfoForm.jsx
+++ src/InfoForm.jsx
@@ -7,6 +7,7 @@ import { styled } from "@mui/system";
 import { Button } from "@mui/material";
 
 const StyledTextField = styled(TextField)`
+  
   .MuiInput-underline::before {
     border-bottom-color: white;
     border-width: 2px;
@@ -26,7 +27,6 @@ const StyledButton = styled(Button)`
   border-color: white;
   border-radius: 150px;
   border-width: 2px;
-  
 
   &:hover {
     background-color: grey;
@@ -40,12 +40,11 @@ const StyledButton = styled(Button)`
 export default function InfoForm() {
   return (
     <div className="bodyBlack">
-      <Grid container spacing={0}>
-        <Grid item xs={12} md={4} />
-        <Grid item xs={12} md={6}>
-          <Typography variant="h1">
+      <Grid container>
+        <Grid item xs={4} md={5} marginLeft="30%">
+          <Typography variant="h1" style={{ fontSize: "6vw" }}>
             <br></br>
-            <b><x></x>
+            <b>
               <span className="span">H</span>
               ave Some Questions
             </b>
@@ -53,76 +52,72 @@ export default function InfoForm() {
         </Grid>
       </Grid>
 
-      <Grid container spacing={0}>
-        <Grid item xs={12} md={4} />
-
-        <Grid item xs={12} md={2}>
-          <StyledTextField
-            fullWidth
-            variant="standard"
-            label="First Name"
-            InputLabelProps={{ className: "customtextfield" }}
-            InputProps={{ className: "customtextfield" }}
-            margin="normal"
-            InputProps={{
-              classes: {
-                root: {
-                  "& .MuiInputBase-input": {
-                    color: "#fff", // Text color
-                  },
-                  "& .MuiInput-underline:before": {
-                    borderBottomColor: "#fff", 
-                  },
-                },
-              },
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} md={1} />
-        <Grid item xs={12} md={2}>
-          <StyledTextField
-            fullWidth
-            variant="standard"
-            label="Last Name"
-            InputLabelProps={{ className: "customtextfield" }}
-            InputProps={{ className: "customtextfield" }}
-            margin="normal"
-          ></StyledTextField>
-        </Grid>
-        <Grid item xs={12} md={2} />
-
-        <Grid item xs={12} md={4} />
-        <Grid item xs={12} md={5}>
-          <StyledTextField
-            fullWidth
-            variant="standard"
-            label="Email"
-            InputLabelProps={{ className: "customtextfield" }}
-            InputProps={{ className: "customtextfield" }}
-            margin="normal"
-          ></StyledTextField>
-        </Grid>
-        <Grid item xs={12} md={4} />
-
-        <Grid item xs={12} md={5}>
-          <StyledTextField
-            fullWidth
-            variant="standard"
-            label="Briefly tell us here"
-            multiline
-            rows={4}
-            InputLabelProps={{ className: "customtextfield" }}
-            InputProps={{ className: "customtextfield" }}
-            margin="normal"
-          ></StyledTextField>
-        </Grid>
-      </Grid>
-      <Grid container direction="column" spacing={5}>
-        <Grid item xs={12} md={6} />
+      <Grid container>
         <Grid item container>
-          <Grid item xs={6} md={6} />
-          <Grid item xs={5} md={3}>
-            <StyledButton variant="outlined" style={{textTransform:"none"}}> Send</StyledButton>
+          <Grid item xs={2} md={3} />
+
+          <Grid item xs={7} md={2}>
+            <StyledTextField
+              fullWidth
+              variant="standard"
+              label="First Name"
+              InputLabelProps={{ className: "customtextfield" }}
+              
+              InputProps={{ className: "customtextfield" }}
+              margin="normal"
+              
+            />
+          </Grid>
+          <Grid item xs={3} md={1} />
+          <Grid item xs={2} md={1} />
+          <Grid item xs={7} md={2}>
+            <StyledTextField
+              fullWidth
+              variant="standard"
+              label="Last Name"
+              InputLabelProps={{ className: "customtextfield" }}
+              InputProps={{ className: "customtextfield" }}
+              margin="normal"
+            ></StyledTextField>
+          </Grid>
+        </Grid>
+
+        <Grid item container>
+          <Grid item xs={2} md={3} />
+
+          <Grid item xs={7} md={6}>
+            <StyledTextField
+              fullWidth
+              variant="standard"
+              label="Email"
+              InputLabelProps={{ className: "customtextfield" }}
+              InputProps={{ className: "customtextfield" }}
+              margin="normal"
+            ></StyledTextField>
+          </Grid>
+          <Grid item xs={11} md={2} />
+          <Grid item xs={2} md={3} />
+
+          <Grid item xs={7} md={6}>
+            <StyledTextField
+              fullWidth
+              variant="standard"
+              label="Briefly tell us here"
+              multiline
+              rows={4}
+              InputLabelProps={{ className: "customtextfield" }}
+              InputProps={{ className: "customtextfield" }}
+              margin="normal"
+            ></StyledTextField>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={3} md={5} />
+          <Grid item xs={4} md={3}>
+            <StyledButton variant="outlined" style={{ textTransform: "none" ,marginTop:"10%"}}>
+              {" "}
+              Send
+            </StyledButton>
           </Grid>
         </Grid>
       </Grid>
